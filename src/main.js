006208c5479fbd7ab1859f50d17cc07d5e7412f5
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-05-15 18:22:20 +0200

    add html classes

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// // london
+// let londonElement = document.querySelector("#london");
+// console.log("londonElement");
+// let londonDateElement = londonElement.document.querySelector(".date");
+// let londonTimeElement = londonElement.document.querySelector(".time");
+// londonDateElement.innerHTML = moment().format("MMMM Do DDD");
