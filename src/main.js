3436e43c2fcd32c398ffc9a9d0f3992f0dce0100
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-05-16 11:43:14 +0200

    added JS to time

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
-// // london
-// let londonElement = document.getElementById("london");
-// let londonTimeElement = londonElement.querySelector(".time");
-// let londonDateElement = londonElement.querySelector(".date");
-// console.log(londonTimeElement);
+function updateTime() {
+  // london
+  let londonElement = document.getElementById("london");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTime = moment();
+
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = `${londonTime.format(
+    `HH : mm : ss`
+  )} <small>${londonTime.format("A")}</small> `;
+
+  // paris
+  let parisElement = document.getElementById("paris");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTime = moment();
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = `${parisTime.format(
+    `HH : mm : ss`
+  )} <small>${parisTime.format("A")}</small> `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
